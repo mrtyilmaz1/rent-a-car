7e7a5a059509ade60e5bc4fb65d496eb0c999622
mrtyilmaz1 <muryilmaz95@gmail.com>
2023-12-28 21:49:40 +0300

    added first controller

---
+++ rent-a-car/src/main/java/com/x/rentacar/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.x.rentacar.controller;
+
+import com.x.rentacar.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+}
